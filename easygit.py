1b0bec5d62dc9d9e8f72ee2c6f7843aa84715b4d
Jose <jmgallego@alea-soluciones.com>
2018-02-06 17:24:07 +0100

    all in an array, need to split them, looks awsome

--- easygit.py
+++ easygit.py
@@ -1,11 +1,28 @@
-#!/usr/bin/python
-from blessings import Terminal
-from pick import pick
+# -*- coding: utf-8 -*-
+import os
+repo_status = os.popen("git status --short").read()
 
-title = 'Please choose the files you want to add (press SPACE to mark, ENTER to continue): '
-options = ['README.MD', 'aFile.js', 'a_python_file.py', 'index.html']
-selected = pick(options, title, multi_select=True, min_selection_count=1)
-print(selected)
+#A  .gitignore
+# M command_spike.py
+# M easygit.py
+# ?? concepto.txt
+# ?? git_status_output.log
+# ?? spec.py
+
+
+text = repo_status.split('\n')
+
+print (text)
+
+
+
+# from blessings import Terminal
+# from pick import pick
+
+# title = 'Please choose the files you want to add (press SPACE to mark, ENTER to continue): '
+# options = ['README.MD', 'aFile.js', 'a_python_file.py', 'index.html']
+# selected = pick(options, title, multi_select=True, min_selection_count=1)
+# print(selected)
 
 
 
